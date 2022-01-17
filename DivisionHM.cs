0c68ba974037813f5263972c16a4dbf466d1137c
Harsh Mittal <hamittal@microsoft.com>
2022-01-17 20:12:46 +0530

    division

---
+++ DivisionHM.cs
@@ -0,0 +1,38 @@
+using System;
+public class Solution {
+    public int Divide(int dividend, int divisor) {
+        int ans = 0;
+        bool numpos, denpos;
+        numpos=false;
+        denpos=false;
+        if(dividend==0)
+            return 0;
+        if(dividend>0)
+            numpos=true;
+        if(divisor>0)
+            denpos=true;
+        dividend=numpos?dividend:(dividend*-1);
+        divisor=denpos?divisor:(divisor*-1);
+        if(divisor==1)
+        {
+            if(denpos!=numpos)
+                dividend*=-1;
+            return dividend;
+        }
+        while(dividend>=divisor)
+        {
+            dividend-=divisor;
+            ++ans;
+        }
+        if(numpos!=denpos)
+            ans=ans*-1;
+        return ans;
+    }
+    static public void Main(String[] args)
+    {
+        int dividend=10;
+        int divisor=3;
+        Console.WriteLine(Divide(dividend,divisor));
+    }
+
+}

02d8658e8116e143cb3b65f85dae22b1707d92e4
Jayesh Sinha <jsinha2411@gmail.com>
2022-01-31 21:53:19 +0530

    Time: 16 ms (27.19%), Space: 2.9 MB (96.72%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.go
@@ -0,0 +1,36 @@
+func romanToInt(s string) int {
+    mapRoman := make(map[string]int)
+    mapRoman["I"] = 1
+    mapRoman["V"] = 5
+    mapRoman["X"] = 10
+    mapRoman["L"] = 50
+    mapRoman["C"] = 100
+    mapRoman["D"] = 500
+    mapRoman["M"] = 1000
+    
+    sum := 0
+    for i := 0; i<len(s); i++ {
+        if i+1 < len(s) && string(s[i]) == "I" && string(s[i+1]) == "V" {
+            sum += 4
+            i = i+1
+        } else if i+1 < len(s) && string(s[i]) == "I" && string(s[i+1]) == "X" {
+            sum += 9
+            i = i+1
+        } else if i+1 < len(s) && string(s[i]) == "X" && string(s[i+1]) == "L" {
+            sum += 40
+            i = i+1
+        } else if i+1 < len(s) && string(s[i]) == "X" && string(s[i+1]) == "C" {
+            sum += 90
+            i = i+1
+        } else if i+1 < len(s) && string(s[i]) == "C" && string(s[i+1]) == "D" {
+            sum += 400
+            i = i+1
+        } else if i+1 < len(s) && string(s[i]) == "C" && string(s[i+1]) == "M" {
+            sum += 900
+            i = i+1
+        } else {
+            sum += mapRoman[string(s[i])]
+        }
+    }
+    return sum
+}

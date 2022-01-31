5b935dbb51baf2218f25fd4e06695a6e0ef3b966
Jayesh Sinha <jsinha2411@gmail.com>
2022-01-31 22:04:19 +0530

    Time: 17 ms (21.83%), Space: 2.9 MB (98.91%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.go
+++ 13-roman-to-integer/13-roman-to-integer.go
@@ -10,27 +10,35 @@ func romanToInt(s string) int {
     
     sum := 0
     for i := 0; i<len(s); i++ {
-        if i+1 < len(s) && string(s[i]) == "I" && string(s[i+1]) == "V" {
-            sum += 4
-            i = i+1
-        } else if i+1 < len(s) && string(s[i]) == "I" && string(s[i+1]) == "X" {
-            sum += 9
-            i = i+1
-        } else if i+1 < len(s) && string(s[i]) == "X" && string(s[i+1]) == "L" {
-            sum += 40
-            i = i+1
-        } else if i+1 < len(s) && string(s[i]) == "X" && string(s[i+1]) == "C" {
-            sum += 90
-            i = i+1
-        } else if i+1 < len(s) && string(s[i]) == "C" && string(s[i+1]) == "D" {
-            sum += 400
-            i = i+1
-        } else if i+1 < len(s) && string(s[i]) == "C" && string(s[i+1]) == "M" {
-            sum += 900
-            i = i+1
-        } else {
-            sum += mapRoman[string(s[i])]
+        if i+1 < len(s) {
+           if string(s[i]) == "I" && string(s[i+1]) == "V" {
+                sum += 4
+                i = i+1
+                continue
+            } else if i+1 < len(s) && string(s[i]) == "I" && string(s[i+1]) == "X" {
+                sum += 9
+                i = i+1
+                continue
+            } else if i+1 < len(s) && string(s[i]) == "X" && string(s[i+1]) == "L" {
+                sum += 40
+                i = i+1
+               continue
+            } else if i+1 < len(s) && string(s[i]) == "X" && string(s[i+1]) == "C" {
+                sum += 90
+                i = i+1
+               continue
+            } else if i+1 < len(s) && string(s[i]) == "C" && string(s[i+1]) == "D" {
+                sum += 400
+                i = i+1
+               continue
+            } else if i+1 < len(s) && string(s[i]) == "C" && string(s[i+1]) == "M" {
+                sum += 900
+                i = i+1
+               continue
+            } 
         }
+        sum += mapRoman[string(s[i])]
+        
     }
     return sum
 }

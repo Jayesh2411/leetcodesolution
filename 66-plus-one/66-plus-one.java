bf50b21e2a08154155bc2353710c5318571dbf01
Jayesh Sinha <jsinha2411@gmail.com>
2022-02-04 22:17:05 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (10.67%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int newDigits[] = new int[digits.length + 1];
-        int val = digits[digits.length - 1];
-        if (val + 1 < 10) {
-            digits[digits.length - 1] = val + 1;
-        } else {
-            newDigits[digits.length] = 0;
-            digits[digits.length - 1] = 0;
-            int carry = 1;
-
-            for (int i = digits.length - 2; i >= 0; i--) {
-                if (digits[i] + carry < 10) {
-                    digits[i] = digits[i] + carry;
-                    break;
-                } else {
-                    digits[i] = 0;
-                }
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] + carry < 10) {
+                digits[i] = digits[i] + carry;
+                break;
+            } else {
+                newDigits[i + 1] = 0;
+                digits[i] = 0;
             }
         }
+
         if (digits[0] == 0) {
             newDigits[0] = 1;
             return newDigits;

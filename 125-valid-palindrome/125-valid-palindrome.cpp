999bdad2f2ea0c83f6e486733092991828517958
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-06 08:30:57 +0530

    Time: 13 ms (17.04%), Space: 7.3 MB (90.37%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,38 +1,12 @@
 class Solution {
 public:
-    bool isAlphanumeric(char c)
-    {
-        if((c>='0'&&c<='9')||((c>=97)&&(c<=122)))
-           return true;
-        return false;
-    }
     bool isPalindrome(string s) {
-        transform(s.begin(),s.end(),s.begin(),::tolower);
-        cout<<s;
-        int i,j;
-        i=0;
-        j=s.length()-1;
-        while(i<j)
-        {
-            while(!isAlphanumeric(s.at(i)))
-            {
-                cout<<s.at(i);
-                ++i;
-                if(i==j)
-                    return true;
-            }
-            while(!isAlphanumeric(s.at(j)))
-            {
-                cout<<s.at(j);
-                --j;
-                if(i==j)
-                    return true;
-            }
-            if(s.at(i)!=s.at(j))
-                return false;
-            --j;
-            ++i;
-        }
-        return true;
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
+        while (isalnum(s[i]) == false && i < j) i++; // Increment left pointer if not alphanumeric
+        while (isalnum(s[j]) == false && i < j) j--; // Decrement right pointer if no alphanumeric
+        if (toupper(s[i]) != toupper(s[j])) return false; // Exit and return error if not match
     }
+    
+    return true;
+}
 };

4a12c1e5e7d3bfbff98105a661925eabdf645703
Jayesh Sinha <jsinha2411@gmail.com>
2022-02-07 19:12:46 +0530

    Time: 140 ms (5.86%), Space: 119.7 MB (5.01%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,11 +1,13 @@
 class Solution {
     public String convert(String s, int numRows) {
+       
         int cols = s.length();
-
         String zigZag[][] = new String[numRows][cols];
         int k = 0;
         for (int i = 0; i < cols; i++) {
-
+            if (k >= s.length()) {
+                break;
+            }
             for (int j = 0; j < numRows && k < s.length(); j++) {   //fills the r
                 zigZag[j][i] = String.valueOf(s.charAt(k++));
             }

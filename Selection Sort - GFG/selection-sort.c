d1dc21753c81d78cd9c1f8499b01fd9ef78f67ec
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-09 11:22:23 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.c
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int select(int arr[], int i);
+void selectionSort(int arr[], int n);
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+
+
+
+void selectionSort(int arr[], int n)
+{
+    int i,j;
+    for(i=0;i<n-1;i++)
+        for(j=i+1;j<n;j++)
+            if(arr[i]>arr[j])
+            {
+                arr[i]=arr[i]+arr[j];
+                arr[j]=arr[i]-arr[j];
+                arr[i]=arr[i]-arr[j];
+            }
+    
+  //code here
+}

c5caaeb9059e77ba15dfc6b17ec01297609aff72
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-10 16:53:18 +0530

    Time: 5 ms (14.38%), Space: 7.9 MB (6.00%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,7 +8,15 @@ public:
             return max(nums[0],nums[1]);
         int len=nums.size();
         dp.resize(len+3,-1);
-        return robCalc(nums,0);
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        dp[2]=nums[2]+nums[0];
+        int i;
+        for(i=3;i<len;i++)
+        {
+            dp[i]=max(nums[i]+max(dp[i-2],dp[i-3]),dp[i-1]);
+        }
+        return max(dp[len-1],dp[1]);
     }
     int robCalc(vector<int> nums,int i)
     {

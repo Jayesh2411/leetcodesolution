58092151b7847a6eb77d394d6bc836f9557a1550
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-11 13:27:00 +0530

    Time: 20 ms (71.58%), Space: 17.4 MB (80.74%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> temp(nums);
-        sort(temp.begin(),temp.end());
         int n = nums.size();
-        int min,max;
-        min=floor(n-1)/2;
-        max=n-1;
-        for(int i=0;i<n;i++)
-        {
-            if(i%2==0)
-            {
-                nums[i]=temp[min--];
-            }
-            else
-                nums[i]=temp[max--];
-        }
-    }
+    
+    // Find a median.
+    auto midptr = nums.begin() + n / 2;
+    nth_element(nums.begin(), midptr, nums.end());
+    int mid = *midptr;
+    
+    // Index-rewiring.
+    #define A(i) nums[(1+2*(i)) % (n|1)]
+
+    // 3-way-partition-to-wiggly in O(n) time with O(1) space.
+    int i = 0, j = 0, k = n - 1;
+    while (j <= k) {
+        if (A(j) > mid)
+            swap(A(i++), A(j++));
+        else if (A(j) < mid)
+            swap(A(j), A(k--));
+        else
+            j++;
+    }  }
 };

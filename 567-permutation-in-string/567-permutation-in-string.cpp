9247db38c38da8ead083487f36b07d8eb23f858e
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-13 12:42:57 +0530

    Time: 98 ms (13.40%), Space: 7.4 MB (39.07%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -3,15 +3,41 @@ public:
     bool checkInclusion(string s1, string s2) {
         if(s1.length()>s2.length())
             return false;
-        string first = s1;
-        sort(first.begin(),first.end());
-        string second;
-        for(int i=0;i<(s2.length()-s1.length()+1);i++)
+        if(s1.length()==0)
+            return true;
+        map<char,int> m;
+        int i;
+        for(i=0;i<s1.length();i++)
+            m[s1[i]]++;
+        int first,last;
+        first=0;
+        last=0;
+        i=0;
+        while(i < s2.length())
         {
-            second = s2.substr(i,s1.length());
-            sort(second.begin(),second.end());
-            if(first==second)
-                return true;
+            while(m[s2[i]]!=0 && i < s2.length())
+            {
+                if(first==last)
+                    first=i;
+                last=i+1;
+                if(last-first==s1.length())
+                   return true;
+                m[s2[i]]--;
+                ++i;
+            }
+            while(s2[first]!=s2[last])
+            {
+                cout<<s2[first]<<" "<<m[s2[first]]<<" "<<first;
+                m[s2[first]]++;
+                ++first;
+            }
+            if(first!=last)
+                {
+                cout<<s2[first]<<" "<<m[s2[first]]<<" "<<first;
+                
+                ++first;
+            ++last;}
+            ++i;
         }
         return false;
     }

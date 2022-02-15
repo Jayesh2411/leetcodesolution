195cace6ec6ffe18ee4877be794b6796a7f86ccf
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-15 08:23:56 +0530

    Time: 120 ms (81.71%), Space: 111.6 MB (20.87%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int countRot = 0, minSteps = INT_MAX, c, b;
-        for(int i = 1; i <= 6; i++)
-        {
-            countRot = 0;
-            for(c=0; c < tops.size(); c++)
-            {
-                if(tops[c] != i)
-                    {if(bottoms[c] == i)
-                        {++countRot;}
-                    else
-                        {break;}}
-            }
-            if(c == tops.size() && countRot < minSteps)
-                {minSteps = countRot;}
+        int countA[] = {0,0,0,0,0,0,0}; 
+        int countB[] = {0,0,0,0,0,0,0}; // countB[i] records the occurrence of i in B.
+        int same[] = {0,0,0,0,0,0,0}; // same[k] records the occurrence of k, where k == A[i] == B[i].
+        for (int i = 0; i < tops.size(); ++i) {
+            ++countA[tops[i]];
+            ++countB[bottoms[i]];
+            if (tops[i] == bottoms[i]) { ++same[tops[i]]; }
         }
-        for(int i = 1; i <= 6; i++)
-        {
-            countRot = 0;
-            for(b=0; b < bottoms.size(); b++)
-            {
-                if(bottoms[b] != i)
-                    if(tops[b] == i)
-                        ++countRot;
-                    else
-                        break;
+        for (int i = 1; i < 7; ++i) {
+            if (countA[i] + countB[i] - same[i] >= tops.size()) {
+                return min(countA[i], countB[i]) - same[i];
             }
-            if(b == bottoms.size() && countRot < minSteps)
-                minSteps = countRot;
         }
-    if(minSteps!=INT_MAX)
-     return minSteps;
-    else
         return -1;
     }
 };

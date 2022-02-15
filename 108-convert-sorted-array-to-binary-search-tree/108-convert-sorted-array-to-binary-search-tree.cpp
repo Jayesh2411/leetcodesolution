3bc178db9e82228ef2a95d50dbce5f03ab408ed2
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-15 19:33:27 +0530

    Time: 102 ms (7.21%), Space: 283.8 MB (10.21%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,21 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode *sortedArrayToBST(vector<int> &num) {
-        if(num.size() == 0) return NULL;
-        if(num.size() == 1)
-        {
-            return new TreeNode(num[0]);
-        }
-        
-        int middle = num.size()/2;
-        TreeNode* root = new TreeNode(num[middle]);
-        
-        vector<int> leftInts(num.begin(), num.begin()+middle);
-        vector<int> rightInts(num.begin()+middle+1, num.end());
-        
-        root->left = sortedArrayToBST(leftInts);
-        root->right = sortedArrayToBST(rightInts);
-        
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return createTree(nums,0,nums.size()-1);
+    }
+    TreeNode* createTree(vector<int> nums,int start, int end)
+    {
+        if( start > end )
+            return nullptr;
+        int mid = start + (end - start)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        if(start == end)
+            return root; 
+        root->left = createTree(nums,start,mid-1);
+        root->right = createTree(nums,mid+1,end);
         return root;
     }
 };

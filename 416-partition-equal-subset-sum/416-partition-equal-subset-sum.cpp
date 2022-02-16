0a22a68688e56c61b22d8631d5ffd5d9ac97776a
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-16 21:40:23 +0530

    Time: 106 ms (77.56%), Space: 9.1 MB (92.04%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,28 +6,25 @@ public:
             sum += i;
         if(sum%2!=0)
             return false;
-        bool dp[nums.size()+1][sum/2+1];
-        for(int i = 0; i <= nums.size(); i++)
+        bool prev[sum/2+1],curr[sum/2+1];
+        prev[0]=true;
+        curr[0]=true;
+        for(int j = 1; j <= sum/2; j++)
         {
-            for(int j = 0; j <= sum/2; j++)
-            {
-                if(j == 0)
-                    dp[i][j] = true;
-                if(i == 0)
-                    dp[i][j] = false;
-            }
+           prev[j]=false;
         }
         for(int i = 1; i <= nums.size(); i++)
         {
             for(int j = 1; j <= sum/2; j++)
             {
                 if(nums[i-1] <= j)
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                    curr[j] = prev[j] || prev[j-nums[i-1]];
                 else
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
             }
+            copy(curr,curr+sum/2,prev);
         }
-        return dp[nums.size()][sum/2];
+        return curr[sum/2];
         
             
     }

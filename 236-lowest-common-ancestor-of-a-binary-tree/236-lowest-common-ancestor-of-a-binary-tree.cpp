a1383cbdf2a591597f1e8c16be0d41424f58137b
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-21 08:28:02 +0530

    Time: 74 ms (5.05%), Space: 24.9 MB (5.05%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,13 +8,19 @@
  * };
  */
 class Solution {
+    unordered_map<string,bool> map;
     bool find(TreeNode* root, TreeNode* look)
-    {
+    {   
         if(root==NULL)
             return false;
+        string key = to_string(root->val)+"_"+to_string(look->val);
+        if(map.find(key)!=map.end())
+            return map[key];
         if(root->val == look->val)
-            return true;
-        return find(root->left,look)||find(root->right,look);
+            map.insert({key,true});
+        else
+            map.insert({key,find(root->left,look)||find(root->right,look)});
+        return map[key];
     }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {

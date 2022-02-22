af08c9ac3aecffceda2c6a19886caffde64c2199
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-22 17:32:26 +0530

    Time: 42 ms (82.51%), Space: 32.2 MB (38.60%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,7 +1,28 @@
 class Solution {
+    int findFloor(vector<int> arr,int x)
+    {
+        int low = 0 , high = arr.size() - 1;
+        if( arr[high] <= x )
+            return high;
+        if( arr[0] >= x )
+            return low;
+        while(low <= high - 1)
+        {
+            int mid = low + (high - low)/2;
+            if( arr[mid] == x  || (arr[mid] < x  && arr[mid+1] > x))
+                return mid;
+            if( arr[mid] < x)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return high;
+    }
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {        
-        int n = size(arr), R = lower_bound(begin(arr), end(arr) - k, x) - begin(arr), L = R - 1;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int i = findFloor(arr,x);      
+        int n = size(arr), R = i + 1, L = i;
+        cout<<i;
         while(k--) 
             if(R >= n || L >= 0 && x - arr[L] <= arr[R] - x) L--;
             else R++;

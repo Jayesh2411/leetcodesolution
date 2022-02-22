13e889ad9978bbca9ce9d481bf3ca4c4ae8d4ca8
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-22 17:18:53 +0530

    Time: 55 ms (70.05%), Space: 32.8 MB (36.02%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,19 +1,39 @@
-
 class Solution {
+    int findFloor(vector<int> arr,int x)
+    {
+        int low = 0 , high = arr.size() - 1;
+        if( arr[high] <= x )
+            return high;
+        if( arr[0] >= x )
+            return low;
+        while(low <= high - 1)
+        {
+            int mid = low + (high - low)/2;
+            if( arr[mid] == x  || (arr[mid] < x  && arr[mid+1] > x))
+                return mid;
+            if( arr[mid] < x)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return high;
+    }
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue < pair< int , int > > maxHeap;
-        for(int i : arr)
-        {
-            maxHeap.push( { abs(i-x), i} );
-            if(maxHeap.size() > k)
-                maxHeap.pop();
-        }
+        int i = findFloor(arr,x);
+        int j = i + 1;
         vector<int> result;
-        while(!maxHeap.empty())
+        while(k)
         {
-            result.push_back(maxHeap.top().second);
-            maxHeap.pop();
+            if(i >= 0 && j < arr.size() )
+            {
+                result.push_back( abs(x-arr[i]) > abs(x-arr[j])?arr[j++]:arr[i--] );
+            }
+            else if(i >= 0)
+                result.push_back(arr[i--]);
+            else
+                result.push_back(arr[j++]);
+            --k;
         }
         sort(result.begin(),result.end());
         return result;

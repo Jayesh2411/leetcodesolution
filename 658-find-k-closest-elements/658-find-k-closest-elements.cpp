137cd0b694429d299a1f165411fe75a07251b9ba
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-22 17:28:55 +0530

    Time: 89 ms (36.32%), Space: 31 MB (94.36%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,41 +1,10 @@
 class Solution {
-    int findFloor(vector<int> arr,int x)
-    {
-        int low = 0 , high = arr.size() - 1;
-        if( arr[high] <= x )
-            return high;
-        if( arr[0] >= x )
-            return low;
-        while(low <= high - 1)
-        {
-            int mid = low + (high - low)/2;
-            if( arr[mid] == x  || (arr[mid] < x  && arr[mid+1] > x))
-                return mid;
-            if( arr[mid] < x)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        return high;
-    }
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int i = findFloor(arr,x);
-        int j = i + 1;
-        vector<int> result;
-        while(k)
-        {
-            if(i >= 0 && j < arr.size() )
-            {
-                result.push_back( abs(x-arr[i]) > abs(x-arr[j])?arr[j++]:arr[i--] );
-            }
-            else if(i >= 0)
-                result.push_back(arr[i--]);
-            else
-                result.push_back(arr[j++]);
-            --k;
-        }
-        sort(result.begin(),result.end());
-        return result;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {        
+        int n = size(arr), R = lower_bound(begin(arr), end(arr) - k, x) - begin(arr), L = R - 1;
+        while(k--) 
+            if(R >= n || L >= 0 && x - arr[L] <= arr[R] - x) L--;
+            else R++;
+        return vector<int>(begin(arr) + L + 1, begin(arr) + R);
     }
 };

3986df0590963531af73086195e961993130d298
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-23 05:32:04 +0530

    Time: 72 ms (27.26%), Space: 38.6 MB (25.17%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=77 lang=cpp
+ *
+ * [77] Combinations
+ */
+
+// @lc code=start
+class Solution {
+    vector<vector<int>> result;
+    void combine(vector<int> nums, int start, int k)
+    {
+        if(nums.size()==k)
+        {
+            result.push_back(nums);
+            return;
+        }
+        if(start == nums.size())
+            return;
+        combine(nums,start+1,k);
+        nums.erase(nums.begin()+start);
+        combine(nums,start,k);
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> local;
+        for(int i = 1; i <= n; i++)
+            local.push_back(i);
+        combine(local,0,k);
+        return result;
+    }
+};
+// @lc code=end
+

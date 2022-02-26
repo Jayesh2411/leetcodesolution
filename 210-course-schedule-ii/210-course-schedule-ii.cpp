986fed092eddfecfc519e88791b8e36af0fdcac3
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-27 04:55:08 +0530

    Time: 24 ms (75.85%), Space: 14.9 MB (23.48%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    vector<vector<int>> graph;
+    vector<int> result;
+    bool findCycle(int node, int vis[])
+    {
+        if(vis[node] == 1)
+            return false;
+        vis[node]+=2;
+        for(int v : graph[node] )
+        {
+            if(vis[v] == 2)
+                return true;
+            if(findCycle(v,vis))
+                return true;
+        }
+        vis[node]--;
+        result.push_back(node);
+        return false;
+    }
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int vis[numCourses];
+        memset(vis,0,sizeof(vis));
+        graph.resize(numCourses);
+        for(auto it : prerequisites)
+            graph[it[0]].push_back(it[1]); 
+        for(int i = 0; i < graph.size(); i++)
+        {
+            if(vis[i] == 0 && findCycle(i,vis))
+                return {};
+        }
+        return result;
+    }
+};
+

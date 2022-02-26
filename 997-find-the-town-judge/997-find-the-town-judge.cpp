b12ae3b2f25226db39ec9ca0dd2ea27b9f45ea40
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-26 20:51:14 +0530

    Time: 237 ms (63.52%), Space: 60.8 MB (76.69%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector< pair <int , int> > graph;
-        graph.resize( n , {0,0} );
+        vector< int > graph;
+        graph.resize( n , 0 );
         for (int i = 0; i < trust.size(); i++ )
         {
-            graph[trust[i][0]-1].first++;
-            graph[trust[i][1]-1].second++;
+            graph[trust[i][0]-1]--;
+            graph[trust[i][1]-1]++;
         }
         for( int i = 0; i < n ; i++ )
-            if( graph[i].first == 0 && graph[i].second == n-1 )
+            if( graph[i] == n-1 )
                 return i + 1;
         return -1;
     }

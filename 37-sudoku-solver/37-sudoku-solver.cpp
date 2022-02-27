89b4b6e8739df8b9fd7f558eb813b655049d5379
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-27 22:53:29 +0530

    Time: 2402 ms (5.00%), Space: 335.9 MB (5.02%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,50 +1,62 @@
 class Solution {
-private:
-    int row[9][256], col[9][256], cube[3][3][256];
+    unordered_set<string> row,col,cube;
+    bool sudoku(vector<vector<char>> board, int i, int j, char val)
+    {
+        if(row.find(to_string(i)+"_"+val)!=row.end())
+            return false;
+        if(col.find(to_string(j)+"_"+val)!=col.end())
+            return false;
+        int rowCube = i/3;
+        int colCube = j/3;
+        if(cube.find(to_string(rowCube)+"_"+to_string(colCube)+"_"+val)!=cube.end())
+            return false;
+        return true;
+        
+    }
+    bool solveSudoku(vector<vector<char>>& board, int i, int j)
+    {
+        if(i==9)
+            return true;
+        if(j==9)
+            return solveSudoku(board,i+1,0);
+        if(board[i][j]!='.')
+            return solveSudoku(board,i,j+1);
+        for( char val = '1'; val <= '9'; val++ )
+        {
+            if(sudoku(board,i,j,val))
+            {
+                board[i][j] = val;
+                int rowCube = i/3;
+                int colCube = j/3;
+                row.insert(to_string(i) + "_" + val);
+                col.insert(to_string(j) + "_" + val);
+                cube.insert(to_string(rowCube)+"_"+to_string(colCube)+"_"+val);
+                if(solveSudoku(board,i,j+1))
+                    return true;
+                row.erase(to_string(i) + "_" + val);
+                col.erase(to_string(j) + "_" + val);
+                cube.erase(to_string(rowCube)+"_"+to_string(colCube)+"_" + val);
+                board[i][j] = '.';
+            }
+        }
+        return false;
+    }
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        memset(row,0,sizeof(row));
-        memset(col,0,sizeof(col));
-        memset(cube,0,sizeof(col));
-        
-        // hash the already existing cell values
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] != '.') {
-                    int d = board[r][c];
-                    row[r][d] = 1; col[c][d] = 1; cube[r/3][c/3][d] = 1;
+        for(int r = 0; r < 9; r++ )
+        {
+            for(int c = 0; c < 9; c++ )
+            {
+                if(board[r][c]!='.')
+                {
+                    row.insert(to_string(r) + "_" + board[r][c]);
+                    col.insert(to_string(c) + "_" + board[r][c]);
+                    int rowCube = r/3;
+                    int colCube = c/3;
+                    cube.insert(to_string(rowCube)+"_"+to_string(colCube)+"_"+board[r][c]);
                 }
             }
         }
-        
-        solveSudoku(board, 0, 0);
-    }
-private:
-    bool check(vector<vector<char>>& board, int r, int c, char val)
-    {
-        if(row[r][val] == 1) return false;
-        if(col[c][val] == 1) return false;
-        if(cube[r/3][c/3][val] == 1) return false;
-        return true;
-    }
-    bool solveSudoku(vector<vector<char>> &board, int i, int j)
-    {
-        if(i==9) return true;
-        if(j==9) return solveSudoku(board, i+1, 0);
-        if(board[i][j] != '.') return solveSudoku(board, i, j+1);
-    
-        for(char d='1'; d<='9'; d++)
-        {
-            if(check(board, i, j, d))
-            {
-                board[i][j] = d;
-                row[i][d] = 1; col[j][d] = 1; cube[i/3][j/3][d] = 1;  // hash the digit 'd'
-                if(solveSudoku(board, i, j+1)) return true;
-                board[i][j] = '.';
-                row[i][d] = 0; col[j][d] = 0; cube[i/3][j/3][d] = 0;  // unhash the digit 'd'
-            }
-        }
-    
-        return false;
+        solveSudoku(board,0,0);
     }
 };

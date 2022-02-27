9cf167c1965b3ef57d5d319bd6753bc21aa04501
Harsh Mittal <harcasm@users.noreply.github.com>
2022-02-27 06:20:05 +0530

    Time: 349 ms (52.21%), Space: 56.1 MB (88.86%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,23 +1,18 @@
-bool comp(vector<int> a, vector<int> b)
-{
-    return a[1]<b[1];
-}
+
 class Solution {
+
 public:
-    int scheduleCourse(vector<vector<int>>& courses) {
-        sort(courses.begin(),courses.end(),comp);
-        priority_queue<int> maxHeap;
-        int time = 0;
-        for(auto it : courses)
-        {
-            time+=it[0];
-            maxHeap.push(it[0]);
-            while(time > it[1])
-            {
-                time-=maxHeap.top();
-                maxHeap.pop();   
-            }
+    int scheduleCourse(vector<vector<int>>& C) {
+        sort(C.begin(), C.end(), [](auto &a, auto &b) {return a[1] < b[1];});
+        priority_queue<int> pq;
+        int total = 0;
+        for (auto &course : C) {
+            int dur = course[0], end = course[1];
+            if (dur + total <= end) 
+                total += dur, pq.push(dur);
+            else if (pq.size() && pq.top() > dur)
+                total += dur - pq.top(), pq.pop(), pq.push(dur);
         }
-        return maxHeap.size();
+        return pq.size();
     }
 };

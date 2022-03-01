19c63b4694f806a049e29eff1f01ddaf9438b4d1
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-01 06:37:52 +0530

    Time: 20 ms (98.04%), Space: 71.3 MB (81.67%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -15,30 +15,23 @@ public:
         ListNode* temp = result;
         int carry = 0;
         int sum = 0;
-        while( l1 && l2 )
+        while( l1 || l2 )
         {
-            sum = l1->val + l2->val+carry;
+            int x = 0, y = 0;
+            if(l1)
+            {
+                x = l1->val;
+                l1 = l1->next;
+            }
+            if(l2)
+            {
+                y = l2->val;
+                l2 = l2->next;
+            }
+            sum = x + y + carry;
             carry = sum/10;
             temp->next = new ListNode(sum%10);
             temp = temp->next;
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        while(l1)
-        {
-            sum = l1->val + carry;
-            carry = sum/10;
-            temp->next = new ListNode(sum%10);
-            temp = temp->next;
-            l1 = l1->next;
-        }
-        while(l2)
-        {
-            sum = l2->val + carry;
-            carry = sum/10;
-            temp->next = new ListNode(sum%10);
-            temp = temp->next;
-            l2 = l2->next;
         }
         if(carry)
             temp->next = new ListNode(carry);

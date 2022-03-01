7a23b71f0c2869558436c37945b2e556522adbcb
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-02 00:11:52 +0530

    Time: 24 ms (88.27%), Space: 28.2 MB (24.70%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -16,11 +16,12 @@ public:
     int max_gain(TreeNode* root)
     {
         if(!root)return 0;
-        int l=max(max_gain(root->left),0);
-        int r=max(max_gain(root->right),0);
-        int new_price=root->val+l+r;
+        int l=max_gain(root->left);
+        int r=max_gain(root->right);
+        int temp = max(root->val,root->val+max(l,r));
+        int new_price=max(root->val+l+r,temp);
         max_sum=max(max_sum,new_price);
-        return root->val+max(l,r);
+        return temp;
     }
     int maxPathSum(TreeNode* root) {
         max_gain(root);

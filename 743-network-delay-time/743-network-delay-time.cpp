353ca8f6881495fedde9228f7333faa721adc822
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-04 04:52:14 +0530

    Time: 129 ms (81.09%), Space: 39.7 MB (88.07%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,30 +1,30 @@
+typedef pair<int, int> pii;
+
 class Solution {
-    void dfs(vector<vector<pair<int,int>>> &graph, int k, vector<int> &visited, int ans)
-    {
-        for(int i = 0; i < graph[k].size(); i++)
-        {
-            if(visited[graph[k][i].first] > ans + graph[k][i].second)
-            {
-                visited[graph[k][i].first] = ans + graph[k][i].second;
-                dfs(graph,graph[k][i].first,visited,visited[graph[k][i].first]);
-            }
-        }
-    }
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int ans = 0;
-        vector<vector<pair<int,int>>> graph;
-        graph.resize(n);
-        for(auto a : times)
-        {
-            graph[a[0]-1].push_back({a[1]-1,a[2]});
+        vector<pii> g[n + 1];
+        for (const auto& t : times) {
+            g[t[0]].emplace_back(t[1], t[2]);
         }
-        vector<int> visited(n,INT_MAX);
-        visited[k-1] = 0;
-        dfs(graph,k-1,visited,ans);
-        ans = 0;
-        for(int i : visited)
-            ans = max(ans,i);
-        return ans == INT_MAX? -1: ans;
+        const int inf = 1e9;
+        vector<int> dist(n + 1, inf);
+        dist[k] = 0;
+        priority_queue<pii, vector<pii>, greater<pii> > pq;
+        pq.push({0, k});
+        int u, v, w;
+        while (!pq.empty()) {
+            pii p = pq.top(); pq.pop();
+            u = p.second;
+            for (auto& to : g[u]) {
+                v = to.first, w = to.second;
+                if (dist[v] > dist[u] + w) {
+                    dist[v] = dist[u] + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+        int ans = *max_element(dist.begin() + 1, dist.end());
+        return ans == inf ? -1 : ans;
     }
 };

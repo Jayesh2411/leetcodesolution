818a0c55fa838fca83f5fbd8898a4b5f2b11d1d8
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-05 04:44:46 +0530

    Time: 84 ms (89.38%), Space: 59.1 MB (39.31%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,10 +4,11 @@ public:
         int water = 0;
         int i = 0, j = height.size() - 1;
         while (i < j) {
-            int h = min(height[i], height[j]);
-            water = max(water, (j - i) * h);
-            while (height[i] <= h && i < j) i++;
-            while (height[j] <= h && i < j) j--;
+            water = max(water, (j - i) * min(height[i],height[j]));
+            if(height[i] < height[j])
+                ++i;
+            else
+                --j;
         }
         return water;
     }

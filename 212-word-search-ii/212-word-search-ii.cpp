f155ab0d13aafbacf08fff80db535c0c0fcf97cd
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-05 07:46:51 +0530

    Time: 1184 ms (25.72%), Space: 13.5 MB (16.59%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -55,10 +55,8 @@ public:
         }
         
         for(int i = 0; i < board.size(); i++)
-            for(int j = 0; j < board[0].size(); j++ )
-            {
+            for(int j = 0; j < board[0].size(); j++)
                 dfs(board,node,"",i,j,result);
-            }
                 
         return result;
     }

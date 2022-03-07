e2eaa8dfac81da74d8e6dc9330cee1207d994447
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-07 13:23:15 +0530

    Time: 85 ms (79.58%), Space: 16.6 MB (18.50%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    pair<int,int> ans;
+    void dijkstra(vector<vector<pair<int,int>>> &graph, int i,int threshold)
+    {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> temp(graph.size(),INT_MAX);
+        pq.push({0,i});
+        temp[i] = 0;
+        while(!pq.empty())
+        {
+            int src = pq.top().second;
+            int dist = pq.top().first;
+            pq.pop();
+            for(auto it : graph[src] )
+            {
+                if(temp[it.first] > dist + it.second)
+                {
+                    temp[it.first] = dist + it.second;
+                    pq.push({temp[it.first],it.first});
+                }
+            }
+        }
+        int ctr = 0;
+        for(int j = 0; j < graph.size(); j++)
+            if(temp[j] <= threshold)
+                ++ctr;
+        if(ans.second > ctr || (ans.second == ctr && ans.first < i))
+            ans = {i,ctr};
+    }
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        ans = {0,INT_MAX};
+        vector<vector<pair<int,int>>> graph(n);
+        for(auto it : edges)
+        {
+            graph[it[0]].push_back({it[1],it[2]});
+            graph[it[1]].push_back({it[0],it[2]});
+        }
+        for(int i = 0; i < n; i++)
+            dijkstra(graph,i,distanceThreshold);
+        return ans.first;
+    }
+};

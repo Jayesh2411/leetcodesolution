4fd537393718f7ac4574d3a7df4d826ceaa14fb7
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-09 05:41:36 +0530

    Time: 128 ms (82.94%), Space: 64.6 MB (61.36%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -10,23 +10,16 @@
  * };
  */
 class Solution {
-    int l,h;
-    int inOrder;
-    void inorder(TreeNode* &root)
-    {
-        if(root == NULL)
-            return;
-        inorder(root->left);
-        if(root->val >= l && root->val <= h)
-            inOrder += root->val;
-        inorder(root->right);
-    }
 public:
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        inOrder = 0;
-        l = low;
-        h = high;
-        inorder(root);
-        return inOrder;
+int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root)
+            return 0;
+        if(root->val<low)
+            return rangeSumBST(root->right,low,high);
+        else if(root->val>high)
+            return rangeSumBST(root->left,low,high);
+        else
+            return root->val + rangeSumBST(root->left,low,root->val-1) + rangeSumBST(root->right,root->val+1,high);
     }
+
 };

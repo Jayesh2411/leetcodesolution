facf7860d2d33dc3795df60da970b20a6787173f
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-12 20:22:42 +0530

    Time: 64 ms (13.42%), Space: 25 MB (49.47%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,21 +3,20 @@ public:
     void rotate(vector<int>& nums, int k) {
         if( k == 0 || k == nums.size() || nums.size() < 2 )
             return;
-        unordered_set<int> s;
         int n = nums.size();
         int i = 0;
-        while(s.size() != n)
+        int count = 0;
+        while(count != n)
         {
-            int tempVal = nums[i];
-            int currK = (i+k)%n;
-            while(s.find(currK) == s.end())
+            int hold = nums[i];
+            int currK = i;
+            do
             {
-                s.insert(currK);
-                int hold = nums[currK];
-                nums[currK] = tempVal;
-                tempVal = hold;
-                currK = (currK+k)%n;
-            }
+                int temp = (currK+k)%n;
+                swap(hold,nums[temp]);
+                ++count;
+                currK = temp;
+            }while(currK != i && count != n );
             ++i;
         }
     }

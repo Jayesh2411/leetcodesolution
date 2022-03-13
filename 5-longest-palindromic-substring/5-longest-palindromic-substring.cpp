c05dc1eb1ae43e62d18d6cfbc91d6bd15587bdef
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-13 15:18:18 +0530

    Time: 411 ms (29.80%), Space: 7 MB (92.53%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -17,7 +17,8 @@ public:
         if(s.length() < 2)
             return s;
         pair<int,int> sol = {0,0};
-        vector<int> dp(s.length(),0);
+        int dp[s.length()];
+        memset(dp,0,sizeof(dp));
         int n = s.length();
         for(int i = n; i > 0; i--)
         {

fafbe08ce5d2902c6c3d215dcb1291a1f3804677
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-15 22:04:48 +0530

    Time: 26 ms (68.68%), Space: 10.9 MB (73.12%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        queue<int> open;
+        int closed = 0;
+        string result = "";
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+                open.push(i);
+            else if(s[i] == ')')
+            {
+                if(!open.empty())
+                {
+                    open.pop();
+                }
+                else
+                    s[i] = '*';
+                
+            }
+        }
+        while(!open.empty())
+        {
+            s[open.front()] = '*';
+            open.pop();
+        }
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
+    }
+};

9dbd693382a6e510d9406e373a1ebdf4330ccb0b
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-16 11:39:18 +0530

    Time: 62 ms (5.48%), Space: 6.9 MB (23.74%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.cpp
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -7,11 +7,9 @@ public:
         d = stoi(s.substr(8));
         if(y%100 == 0)
             y = y/100;
-        int leap = y%4==0?1:0;
+        int leap = y%4==0&&m>2?1:0;
         vector<int> days = {0,31,59,90,120,151,181,212,243,273,304,334};
-        int ans = days[m-1] + d;
-        if(leap == 1 && m > 2)
-            ans ++;
+        int ans = days[m-1] + d + leap;
         return ans;
     }
 };

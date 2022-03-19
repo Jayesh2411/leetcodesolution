d4ef2811601fb948dbff49fd8549b8d03c309170
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-19 20:52:52 +0530

    Time: 3 ms (63.88%), Space: 8.8 MB (36.18%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int j = 0;
-        for(auto x : nums)
-            if(x != val)
-                nums[j++] = x;
-        return j;
+        return std::distance(nums.begin(), std::remove(nums.begin(), nums.end(), val));
     }
 };

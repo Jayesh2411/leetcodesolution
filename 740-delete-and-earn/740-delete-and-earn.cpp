6f9c586052cd46aff0d05b331d042c2fcf02cbf2
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-30 06:30:58 +0530

    Time: 12 ms (53.87%), Space: 13 MB (31.58%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int,int> m;
+        map<int,int> m;
         int maxEl = 0;
+        int minEl = INT_MAX;
         for(int i: nums)
         {
             m[i]++;
             maxEl = max(maxEl,i);
+            minEl = min(minEl,i);
         }
-        int ans = 0, min1 = 0, min2 = 0, i = 1;
-        while( i <= maxEl )
+        int ans = 0, min1 = 0, min2 = 0;
+        while( minEl <= maxEl )
         {
-            if(m[i] != 0)
+            if(m[minEl] != 0)
             {
-                ans = max(i*m[i]+min2,min1);
+                ans = max(minEl*m[minEl]+min2,min1);
                 min2 = min1;
                 min1 = ans;
             }
             else
                 min2 = min1;
-            ++i;
+            ++minEl;
         }
         return ans;        
     }

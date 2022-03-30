c3b34edd5a607767dff05932cc2af521138599f6
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-30 06:54:39 +0530

    Time: 86 ms (62.64%), Space: 48.4 MB (74.03%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size() == 1 || nums[0] >= nums.size() - 1)
+            return true;
+        int maxL = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(i <= maxL)
+            {
+                if(nums[i]+i > maxL)
+                {
+                    maxL = nums[i] + i;
+                    if(maxL >= nums.size() - 1)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};

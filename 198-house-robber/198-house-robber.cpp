32a8c503d40d15db7469ec857bc5fec67c44faaa
Harsh Mittal <harcasm@users.noreply.github.com>
2022-03-30 06:24:52 +0530

    Time: 4 ms (34.75%), Space: 7.7 MB (47.71%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     
     int rob(vector<int>& nums) {
-        int n = nums.size();
         int a = 0;
         int b = 0;
-
-        for (int i=0; i<n; i++)
+        for (int i=0; i< nums.size(); i++)
         {
             if (i%2==0)
             {

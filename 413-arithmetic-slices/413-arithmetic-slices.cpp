2e84e1a00a68fcf77391eff2e907c77d8de00a5e
Harsh Mittal <harcasm@users.noreply.github.com>
2022-04-01 11:18:56 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (48.32%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -4,16 +4,11 @@ public:
         if(nums.size() < 3)
             return 0;
         int ans = 0;
-        vector<int> diff(nums.size()-1);
-        for(int i = 0; i < nums.size()-1; i++)
-        {
-            diff[i] = nums[i+1]-nums[i];
-        }
         int i = 0;
         while(i < nums.size() - 2)
         {
             int temp = i;
-            while(temp < nums.size() - 1 && diff[temp] == diff[i])
+            while(temp < nums.size() - 1 && nums[temp+1]-nums[temp] == nums[i+1]-nums[i])
                 ++temp;
             if(temp != i+1)
             {

c739fcd0e6f82818661d0358397c8396ea933a13
Harsh Mittal <harcasm@users.noreply.github.com>
2022-04-01 06:47:19 +0530

    Time: 20 ms (46.03%), Space: 14.4 MB (26.55%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,6 +1,5 @@
 class Solution {
-    vector<bool> dp;
-    bool findTruth(string s, int start, unordered_set<string> &w)
+    bool findTruth(string s, int start, unordered_set<string> &w,vector<bool> &dp)
     {
         if(s.size() == start)
             return true;
@@ -10,7 +9,7 @@ class Solution {
         {
             if(w.find(s.substr(start,i+1-start)) != w.end())
             {
-                if(findTruth(s,i+1,w) == true)
+                if(findTruth(s,i+1,w,dp) == true)
                     return true;
                 else
                     dp[i+1] = false;
@@ -20,10 +19,11 @@ class Solution {
     }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp;
         dp.resize(s.length()+1,true);
         unordered_set<string> w;
         for(auto x : wordDict)
             w.insert(x);
-        return findTruth(s,0,w);
+        return findTruth(s,0,w,dp);
     }
 };

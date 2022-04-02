53662060e930a06c7796f0552a6d9aa8d1e0ce95
Harsh Mittal <harcasm@users.noreply.github.com>
2022-04-02 10:08:08 +0530

    Time: 4 ms (92.22%), Space: 8.6 MB (67.14%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -6,10 +6,10 @@ public:
         int ans = INT_MAX;
         for(int i = 1; i < triangle.size(); i++)
         {
-            int temp = triangle[i-1].size();
+            int temp = triangle[i-1].size()-1;
             for(int j = 0; j <= i; j++)
             {
-                triangle[i][j] += min(triangle[i-1][max(j-1,0)],triangle[i-1][min(j,temp-1)]);
+                triangle[i][j] += min(triangle[i-1][max(j-1,0)],triangle[i-1][min(j,temp)]);
                 if(i == triangle.size()-1)
                     ans = min(ans,triangle[i][j]);
             }

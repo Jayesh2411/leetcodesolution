97fb3feb1c33152b86e67a8141c3e724a8e428f7
Harsh Mittal <harcasm@users.noreply.github.com>
2022-04-02 10:19:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.10%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> arr(m,vector<int>(n,1));
+        int arr[m][n];
         if(n==0)
             return m;
         if(m==0)
             return n;
-        for(int i=1;i<m;i++)
+        --m;
+        --n;        
+        for(int i=0;i<=m;i++)
         {
-            for(int j=1;j<n;j++)
+            arr[i][0]=1;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            arr[0][i]=1;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
             {
                 arr[i][j]=arr[i-1][j]+arr[i][j-1];
             }
         }
-        return arr[m-1][n-1];
+        return arr[m][n];
     }
 };

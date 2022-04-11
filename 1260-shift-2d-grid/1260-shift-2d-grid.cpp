92bb27ac732e0f7913f9c42360211ff8e6c1663b
Harsh Mittal <harcasm@users.noreply.github.com>
2022-04-11 10:04:38 +0530

    Time: 28 ms (79.20%), Space: 14 MB (73.22%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -5,12 +5,8 @@ public:
         int m = grid.size(), n = grid[0].size();
         ans.resize(m, vector<int> (n));
         for(int i = 0; i < m; i++)
-        {
             for(int j = 0; j < n; j++)
-            {
                 ans[(i+(j+k)/n)%m][(j+k)%n] = grid[i][j];
-            }
-        }
         return ans;
     }
 };

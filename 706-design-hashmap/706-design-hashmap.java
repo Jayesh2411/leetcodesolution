53ddfff242924524cf70c6ec1dc1082cf7b7e19b
Jayesh Sinha <jsinha2411@gmail.com>
2022-04-22 22:56:18 +0530

    Time: 100 ms (17.70%), Space: 110.7 MB (9.02%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,39 +1,24 @@
 class MyHashMap {
-    ArrayList<Integer> indices;
-    
-    public MyHashMap() {
-        this.indices = new ArrayList<>();
-        for (int i = 0;i<=1000001;i++) {
-            indices.add(i,null);
-        }
-    }
-    
-    public void put(int key, int value) {
-             indices.set(key,value); 
-    }
-    
-    public int get(int key) {
-        if( indices.get(key) == null) {
-            return -1;
-        }
-        return indices.get(key);
-    }
-    
-    public void remove(int key) {
-        indices.set(key,null);
-    }
+
+   int[] arr;
+   public MyHashMap() {        
+    arr=new int[1000001];    
+       Arrays.fill(arr,-1);
+   }
+   
+   public void put(int key, int value) {
+       arr[key]=value;
+   }
+   
+   public int get(int key) {
+       return arr[key];
+   }
+   
+   public void remove(int key) {
+       arr[key]=-1;
+   }
 }
-class Node {
-    int key;
-    int val;
-    public Node() {
-        
-    }
-    public Node(int key, int val) {
-        this.key = key;
-        this.val = val;
-    }
-}
+
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap obj = new MyHashMap();

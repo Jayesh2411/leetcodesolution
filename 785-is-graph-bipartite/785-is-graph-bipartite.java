34e7a34bbb7a207ba9ed894951393a4d062189bf
Jayesh Sinha <jsinha2411@gmail.com>
2022-05-04 15:22:18 +0530

    Time: 1 ms (81.30%), Space: 54 MB (29.63%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -4,23 +4,24 @@ class Solution {
         boolean[] isVisited = new boolean[graph.length];
         boolean[] colored = new boolean[graph.length];
         for (int i = 0;i<graph.length;i++) {
-            ArrayDeque<Integer> queue = new ArrayDeque<>();
-            queue.offer(i);
-            
-            while(!queue.isEmpty()) {
-                int node = queue.poll();
-                isVisited[node] = true;
-                for (int next:graph[node]) {
-                    if(isVisited[next]) {
-                        if(colored[next] == colored[node] ) {
-                            return false;
-                        }
-                    } else {
-                        queue.offer(next);
-                        
-                        colored[next] = !colored[node] ;
-                    }
+            if( !dfs(graph, i, isVisited, colored)){
+                return false;
+            }
+        }
+        return true;
+    }
+    boolean dfs(int[][] graph, int i, boolean[] isVisited, boolean[] colored) {
+        if (isVisited[i]) return true;
+       
+        isVisited[i]=true;
+        for (int next:graph[i]) {
+            if (isVisited[next]) {
+                if (colored[next] == colored[i]) {
+                    return false;
                 }
+            } else {
+                colored[next] = !colored[i];
+                if (!dfs(graph, next, isVisited, colored)) return false;
             }
         }
         return true;

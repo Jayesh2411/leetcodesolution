fa20731f4c0995ed82b80f7f1729b6a1b1153afd
Jayesh Sinha <jsinha2411@gmail.com>
2022-05-15 20:02:26 +0530

    Time: 1 ms (92.05%), Space: 43.2 MB (16.50%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,5 +1,5 @@
 class Solution {
-     public static List<String> letterCombinations(String digits) {
+     public static List<String> letterCombinations1(String digits) {
         if (digits == null || digits.length() == 0) {
             return new ArrayList<>();
         }
@@ -22,4 +22,28 @@ class Solution {
 
         return new ArrayList<>(queue);
     }
+    static String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public static List<String> letterCombinations(String digits) {
+        if (digits.trim().isEmpty() || digits == null) {
+            return Collections.emptyList();
+        }
+        
+        List<String> combinations = new ArrayList<>();
+        
+        compute(combinations, "", 0, digits);
+        return combinations;
+    }
+    
+    static void compute(List<String> combinations , String val , int index, String digits) {
+        if (index >= digits.length()) {
+            combinations.add(val);
+            return;
+        }
+        
+        String s = letters[(digits.charAt(index) - '0')-2];
+        for (int i=0;i<s.length();i++) {
+            compute(combinations, val+String.valueOf(s.charAt(i)), index+1, digits);
+        }
+    }
+    
 }
